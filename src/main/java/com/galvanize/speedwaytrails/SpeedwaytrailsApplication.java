9ab555eaced7a59f4f0b71ba7dde6be596727c02
nagarjunaboddu <nagarjuna.boddu@cognizant.com>
2021-02-08 18:53:05 -0500

    Initial commit

---
+++ src/main/java/com/galvanize/speedwaytrails/SpeedwaytrailsApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.speedwaytrails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedwaytrailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedwaytrailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/speedwaytrails/SpeedwaytrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.speedwaytrails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedwaytrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
