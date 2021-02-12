046c5c82dc7ed9c767ac3c6396dda97c7c733aff
subhrajit <subhrajit.banerjee.official>
2021-02-11 19:53:55 -0500

    implemented PostGreSQL from cloud

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO owner (id, first_name, last_name, age, nickname,wins,losses) VALUES (1, 'Maria', 'Lopez', 22, 'The Blaze', 4, 1);
+
+INSERT INTO race_car (id, nickname, model, year, status,top_speed, owner_id) VALUES (11, 'The Condor', 'Corvette', '2019', 'AVAILABLE',190, 1);
+INSERT INTO race_car (id, nickname, model, year, status,top_speed, owner_id) VALUES (14, 'Red Fire', 'Ferrari', '2019','AVAILABLE',223, 1);
