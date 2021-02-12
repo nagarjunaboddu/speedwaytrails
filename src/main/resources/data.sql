25622916ac234361397215b7f36027754988dbf8
subhrajit <subhrajit.banerjee.official>
2021-02-11 19:58:50 -0500

    implemented PostGreSQL from cloud

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO owner (id, first_name, last_name, age, nickname,wins,losses) VALUES (1, 'Maria', 'Lopez', 22, 'The Blaze', 4, 1);
+INSERT INTO owner (id, first_name, last_name, age, nickname,wins,losses) VALUES (22, 'Maria', 'Lopez', 22, 'The Blaze', 4, 1);
 
-INSERT INTO race_car (id, nickname, model, year, status,top_speed, owner_id) VALUES (11, 'The Condor', 'Corvette', '2019', 'AVAILABLE',190, 1);
-INSERT INTO race_car (id, nickname, model, year, status,top_speed, owner_id) VALUES (14, 'Red Fire', 'Ferrari', '2019','AVAILABLE',223, 1);
+INSERT INTO race_car (id, nickname, model, year, status,top_speed, owner_id) VALUES (111, 'The Condor', 'Corvette', '2019', 'AVAILABLE',190, 22);
+INSERT INTO race_car (id, nickname, model, year, status,top_speed, owner_id) VALUES (141, 'Red Fire', 'Ferrari', '2019','AVAILABLE',223, 22);
